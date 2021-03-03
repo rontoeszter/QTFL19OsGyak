4caa236dc928af57684fff6b8a4885470b60e2d9
REszter <78937229+rontoeszter@users.noreply.github.com>
2021-03-03 12:45:07 +0100

    Add files via upload

---
+++ QTFL19_0224/QTFL19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *text;
+    text = fopen("Rontó.txt", "r");
+    fprintf(text, "Rontó Eszter, Programtervezõ info, QTFL19");
+    int fclose( FILE *text);
+    return 0;
+}

3b1937f726700e973eb5034c058166d61808bcba
REszter <78937229+rontoeszter@users.noreply.github.com>
2021-04-07 11:33:08 +0200

    Add files via upload

--- QTFL19_0331/QTFL19_alarm.c
+++ QTFL19_0331/QTFL19_alarm.c
@@ -1,1 +1,30 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+void do_nothing();
+void do_break();
+
+int main(){
+	int i;
+	unsigned int sec = 1;
+	signal(SIGINT, do_break);
+
+	for(i = 1; i < 8; i++){
+	alarm(sec);
+	signal(SIGALRM, do_nothing);
+	printf("%d varok\n", i);
+	pause();
+	}
+
+return 0;
+}
+
+void do_nothing(){
+	printf("ALARM\n"); }
+
+
+void do_break(){
+	printf("Megszakitas!\n");
+	signal(SIGINT, SIG_IGN); }
 
